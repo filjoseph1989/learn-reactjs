f4c879dc14ec2445b05871dda59bcc039b00c0f0
Fil <filjoseph22@gmail.com>
2021-04-22 07:53:00 +0800

    sample stripe payment inplementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import  spatula from './assets/CaHphgF.png';
+import StripeContainer from './components/StripeContainer'
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showItem, setShowItem] = useState(false)
+
+    return (
+        <div className="App">
+            <h1>The Spatula Store</h1>
+            {showItem ? <StripeContainer /> : <>
+                <h3>$10.00</h3>
+                <img src={spatula} alt="Spaluta" />
+                <button onClick={() => setShowItem(true)}>Purchased Spatula</button>
+            </>}
+        </div>
+    );
 }
 
 export default App;
