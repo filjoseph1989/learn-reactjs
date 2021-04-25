b913ea3cca63ca4af411bdef85af825987983d93
Fil <fil.prospark@gmail.com>
2021-04-26 07:46:35 +0800

    passing data using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem () {
+function ExpenseItem (props) {
     return (
-        <div className="bg-purple-300 border grid m-5 mt-5 p-5 rounded w-1/2">
+        <div className="bg-purple-300 border grid m-5 mt-5 p-5 rounded">
             <div className="grid grid-cols-3">
-                <div className="flex font-bold items-center justify-start text-lg">April 21, 2021</div>
-                <div className="flex font-bold items-center justify-start text-lg">Insurance</div>
-                <div className="border flex font-bold justify-center p-2 rounded"> $100.00 </div>
+                <div className="flex font-bold items-center justify-start text-lg">
+                    <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="flex font-bold items-center justify-start text-lg">{props.title}</div>
+                <div className="border flex font-bold justify-center p-2 rounded">${props.amount}</div>
             </div>
         </div>
     );
