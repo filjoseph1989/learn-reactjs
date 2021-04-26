3a4e18ebc729d042bb63cabe7a1b8e489a5fc69c
Fil <fil.prospark@gmail.com>
2021-04-26 14:37:15 +0800

    splitting components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import './App.css';
 import Expenses from './components/Expenses';
 
 function App() {
+    const data = [
+        {
+            title: 'Car Insurance',
+            amount: 299.00,
+            date: new Date(2018, 11, 24)
+        },
+        {
+            title: 'House Insurance',
+            amount: 499.00,
+            date: new Date(2018, 11, 24)
+        },
+        {
+            title: 'Life Insurance',
+            amount: 999.00,
+            date: new Date(2018, 11, 24)
+        }
+    ];
+
     return (
         <div className="App">
-            <Expenses />
+            <Expenses items={data} />
         </div>
     );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,29 +1,11 @@
 import ExpenseItem from './ExpenseItem';
 
-function Expenses() {
-    const data = [
-        {
-            title: 'Car Insurance',
-            amount: 299.00,
-            date: new Date(2018, 11, 24)
-        },
-        {
-            title: 'House Insurance',
-            amount: 499.00,
-            date: new Date(2018, 11, 24)
-        },
-        {
-            title: 'Life Insurance',
-            amount: 999.00,
-            date: new Date(2018, 11, 24)
-        }
-    ];
-
+function Expenses(props) {
     return (
         <header className="App-header grid mx-auto w-1/2">
-            <ExpenseItem title={data[0].title} amount={data[0].amount} date={data[0].date} />
-            <ExpenseItem title={data[1].title} amount={data[1].amount} date={data[1].date} />
-            <ExpenseItem title={data[2].title} amount={data[2].amount} date={data[2].date} />
+            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
+            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
+            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
         </header>
     );
 }
