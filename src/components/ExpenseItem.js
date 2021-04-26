1addbcedd24d3c59cbe31e6d4e4e6360d96ce198
Fil <fil.prospark@gmail.com>
2021-04-26 18:01:07 +0800

    implement composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 // import './ExpenseItem.css'; to be able to add css just do this line and create file of it
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem (props) {
     return (
-        <div className="bg-purple-300 border grid m-5 mt-5 p-5 rounded">
+        <Card className="bg-purple-300 border grid m-5 mt-5 p-5 rounded card">
             <div className="grid grid-cols-3">
                 <ExpenseDate date={props.date} />
                 <div className="flex font-bold items-center justify-start text-lg">{props.title}</div>
                 <div className="border flex font-bold items-center justify-center p-2 rounded">${props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
