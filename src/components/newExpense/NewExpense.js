d40335d590154f944f51ce4adbe8aae427e2ab88
Fil <filjoseph22@gmail.com>
2021-05-19 09:05:48 +0800

    removed other files

--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,7 +1,8 @@
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
-    return <div className="rounded border grid mt-11 mx-auto p-5 w-1/2">
+    return
+    <div className="rounded border grid mt-11 mx-auto p-5 w-1/2">
         <ExpenseForm onSaveExpenseData={props.onAddExpense} />
     </div>
 }
